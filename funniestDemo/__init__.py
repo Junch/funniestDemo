db4d81fc0207dc1f4f0dd13b2a654a7eace13bac
Jun Chen <junc76@gmail.com>
2022-04-18 09:18:54 +0800

    print the buddha.txt

--- funniestDemo/__init__.py
+++ funniestDemo/__init__.py
@@ -1,12 +1,10 @@
 import sys
+from importlib import resources
 print('__init__')
 print('__init__.__name__', __name__)
 print('__init__.__package__', __package__)
 print('sys.path', sys.path)
 
 def joke():
-    print (u'How do you tell HTML from HTML5?'
-            u'Try it out in Internet Explorer.'
-            u'Does it work?'
-            u'No?'
-            u'It\'s HTML5.')
+    with resources.open_text('funniestDemo', 'buddha.txt') as f:
+        print(f.read())
